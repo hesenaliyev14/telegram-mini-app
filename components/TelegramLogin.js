38c606bdfa5f58c5e833a35b40c641b2779246d0
hesenaliyev14 <hesenaliyevis@gmail.com>
2024-11-10 00:56:57 +0400

    some changes

--- components/TelegramLogin.js
+++ components/TelegramLogin.js
@@ -5,7 +5,7 @@ const TelegramLogin = ({ onAuth }) => {
     // Append Telegram Login widget script to the page
     const script = document.createElement('script');
     script.src = 'https://telegram.org/js/telegram-widget.js?22';
-    script.setAttribute('data-telegram-login', 'gencedenHesenBot');
+    script.setAttribute('data-telegram-login', 'hesenAuthBot');
     script.setAttribute('data-size', 'large');
     script.setAttribute('data-auth-url', `${process.env.NEXT_PUBLIC_AUTH_URL}/api/auth/telegram`);
     script.setAttribute('data-request-access', 'write');
